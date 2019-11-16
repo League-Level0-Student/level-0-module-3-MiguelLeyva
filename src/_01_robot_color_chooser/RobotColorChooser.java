688745d754f52977c4c4dea8aa501d59451e9d63
ilabstaff <ilabstaff@172.16.116.68>
2019-11-16 11:25:25 -0800

    RobotColorChooser

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,10 +4,69 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		
+		Robot r2d2 = new Robot();
+		r2d2.setPenWidth(10);
+		for (int i = 0; i < 10; i++) {
+			
+		
+		String color = JOptionPane.showInputDialog("what color do you want... blue, green, or red?");
+		
+		if(color.equals("blue")){
+			r2d2.setPenColor(Color.BLUE);
+		}
+		else if(color.equals("green")) {
+			r2d2.setPenColor(Color.GREEN);
+		}
+		else if(color.equals("red")) {
+			r2d2.setPenColor(Color.RED);
+		}
+		else {
+			r2d2.setRandomPenColor();
+		}
+		
+		r2d2.setSpeed(200);
+		r2d2.turn(180);
+		r2d2.move(270);
+		r2d2.penDown();
+		r2d2.turn(90);
+		r2d2.move(440);
+		r2d2.turn(90);
+		r2d2.move(560);
+		r2d2.turn(90);
+		r2d2.move(580);
+		r2d2.turn(90);
+		r2d2.move(520);
+		r2d2.turn(90);
+		r2d2.move(520);
+		r2d2.turn(90);
+		r2d2.move(490);
+		r2d2.turn(90);
+		r2d2.move(480);
+		r2d2.turn(90);
+		r2d2.move(460);
+		r2d2.turn(90);
+		r2d2.move(430);
+		r2d2.turn(90);
+		r2d2.move(430);
+		r2d2.turn(90);
+		r2d2.move(400);
+		r2d2.turn(90);
+		r2d2.move(390);
+		r2d2.turn(90);
+		r2d2.move(380);
+		r2d2.turn(90);
+		r2d2.move(350);
+		r2d2.turn(90);
+		r2d2.move(330);
+		}
 		//1. Create a new Robot
 		
 		//3. Ask the user what color they would like the robot to draw
